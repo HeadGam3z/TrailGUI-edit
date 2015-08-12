0bd78d62f256b1929b05d9b5b79072245ae1c838
Jesse McCullough <jesse.mccullough@hotmail.com>
2015-08-12 15:56:41 -0400

    Used Auto-Indention to make the code a little bit easier to read.

--- TrailGUI/src/jamiesinn/trailgui/Main.java
+++ TrailGUI/src/jamiesinn/trailgui/Main.java
@@ -10,64 +10,61 @@ import org.bukkit.plugin.java.JavaPlugin;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Main
-        extends JavaPlugin
-{
-    public static Main plugin;
-    public static List<String> trailAngryVillager = new ArrayList<String>();
-    public static List<String> trailCloud = new ArrayList<String>();
-    public static List<String> trailCriticals = new ArrayList<String>();
-    public static List<String> trailDripLava = new ArrayList<String>();
-    public static List<String> trailDripWater = new ArrayList<String>();
-    public static List<String> trailEnchantment = new ArrayList<String>();
-    public static List<String> trailSpark = new ArrayList<String>();
-    public static List<String> trailFlame = new ArrayList<String>();
-    public static List<String> trailHappyVillager = new ArrayList<String>();
-    public static List<String> trailInstantSpell = new ArrayList<String>();
-    public static List<String> trailLargeSmoke = new ArrayList<String>();
-    public static List<String> trailLava = new ArrayList<String>();
-    public static List<String> trailMagicCrit = new ArrayList<String>();
-    public static List<String> trailMobSpell = new ArrayList<String>();
-    public static List<String> trailMobSpellAmbient = new ArrayList<String>();
-    public static List<String> trailNote = new ArrayList<String>();
-    public static List<String> trailPortal = new ArrayList<String>();
-    public static List<String> trailRedDust = new ArrayList<String>();
-    public static List<String> trailColoredRedDust = new ArrayList<String>();
-    public static List<String> trailSlime = new ArrayList<String>();
-    public static List<String> trailSnowShovel = new ArrayList<String>();
-    public static List<String> trailSnowballPoof = new ArrayList<String>();
-    public static List<String> trailSpell = new ArrayList<String>();
-    public static List<String> trailSplash = new ArrayList<String>();
-    public static List<String> trailTownAura = new ArrayList<String>();
-    public static List<String> trailWake = new ArrayList<String>();
-    public static List<String> trailWitchMagic = new ArrayList<String>();
-    public static List<String> trailHearts = new ArrayList<String>();
-    public static List<String> trailEnderSignal = new ArrayList<String>();
-    public static List<String> trailIconCrack = new ArrayList<String>();
-    public static List<String> trailBlockBreak = new ArrayList<String>();
+public class Main extends JavaPlugin {
+	public static Main plugin;
+	public static List<String> trailAngryVillager = new ArrayList<String>();
+	public static List<String> trailCloud = new ArrayList<String>();
+	public static List<String> trailCriticals = new ArrayList<String>();
+	public static List<String> trailDripLava = new ArrayList<String>();
+	public static List<String> trailDripWater = new ArrayList<String>();
+	public static List<String> trailEnchantment = new ArrayList<String>();
+	public static List<String> trailSpark = new ArrayList<String>();
+	public static List<String> trailFlame = new ArrayList<String>();
+	public static List<String> trailHappyVillager = new ArrayList<String>();
+	public static List<String> trailInstantSpell = new ArrayList<String>();
+	public static List<String> trailLargeSmoke = new ArrayList<String>();
+	public static List<String> trailLava = new ArrayList<String>();
+	public static List<String> trailMagicCrit = new ArrayList<String>();
+	public static List<String> trailMobSpell = new ArrayList<String>();
+	public static List<String> trailMobSpellAmbient = new ArrayList<String>();
+	public static List<String> trailNote = new ArrayList<String>();
+	public static List<String> trailPortal = new ArrayList<String>();
+	public static List<String> trailRedDust = new ArrayList<String>();
+	public static List<String> trailColoredRedDust = new ArrayList<String>();
+	public static List<String> trailSlime = new ArrayList<String>();
+	public static List<String> trailSnowShovel = new ArrayList<String>();
+	public static List<String> trailSnowballPoof = new ArrayList<String>();
+	public static List<String> trailSpell = new ArrayList<String>();
+	public static List<String> trailSplash = new ArrayList<String>();
+	public static List<String> trailTownAura = new ArrayList<String>();
+	public static List<String> trailWake = new ArrayList<String>();
+	public static List<String> trailWitchMagic = new ArrayList<String>();
+	public static List<String> trailHearts = new ArrayList<String>();
+	public static List<String> trailEnderSignal = new ArrayList<String>();
+	public static List<String> trailIconCrack = new ArrayList<String>();
+	public static List<String> trailBlockBreak = new ArrayList<String>();
 
-    public static Main getPlugin()
-    {
-        return plugin;
-    }
+	public static Main getPlugin() {
+		return plugin;
+	}
 
-    @Override
-    public void onEnable()
-    {
-        getServer().getPluginManager().registerEvents(new Listeners(this), this);
-        getCommand("Trail").setExecutor(new Trail(this));
-        getCommand("Trails").setExecutor(new Trails(this));
-        getCommand("TrailGUI").setExecutor(new TrailGUI(this));
+	@Override
+	public void onEnable() {
+		getServer().getPluginManager()
+				.registerEvents(new Listeners(this), this);
+		getCommand("Trail").setExecutor(new Trail(this));
+		getCommand("Trails").setExecutor(new Trails(this));
+		getCommand("TrailGUI").setExecutor(new TrailGUI(this));
 
-        getConfig().options().copyDefaults(true);
-        saveDefaultConfig();
+		getConfig().options().copyDefaults(true);
+		saveDefaultConfig();
 
-        plugin = this;
-        TrailData.createFile();
-        TrailData.config = YamlConfiguration.loadConfiguration(TrailData.file);
-    }
+		plugin = this;
+		TrailData.createFile();
+		TrailData.config = YamlConfiguration.loadConfiguration(TrailData.file);
+	}
 
-    @Override
-    public void onDisable()
-    {}
+	@Override
+	public void onDisable() {
+	}
 }
